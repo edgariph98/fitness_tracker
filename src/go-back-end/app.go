83527b3a7eab2197d2767377408c04763118a2b5
Edgar Portales <edgar_ivanhdz15@hotmail.com>
2022-05-07 17:40:48 +0000

    update port for backend comment

--- src/go-back-end/app.go
+++ src/go-back-end/app.go
@@ -9,5 +9,5 @@ func main() {
 			"message": "pong",
 		})
 	})
-	r.Run(":5000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(":5000") // listen and serve on port 5000
 }
